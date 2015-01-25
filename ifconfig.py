45d9d81e089e0db0f3406c29fd663a7ac489bd3a
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2015-01-25 15:44:14 +0100

    Remove daemon

--- ifconfig.py
+++ ifconfig.py
@@ -1,9 +1,5 @@
-from daemon import DaemonContext
-from daemon.pidfile import TimeoutPIDLockFile
 from flask import Flask, request, Response
-from os.path import abspath, dirname, join
 
-pidfile_path = join(abspath(dirname(__file__)), 'ifconfig-py.pid')
 app = Flask(__name__)
 
 
@@ -16,5 +12,4 @@ def remote_addr():
     return Response(ip, mimetype='text/plain; charset=utf-8')
 
 if __name__ == '__main__':
-    with DaemonContext(pidfile=TimeoutPIDLockFile(pidfile_path)):
-        app.run()
+    app.run()
