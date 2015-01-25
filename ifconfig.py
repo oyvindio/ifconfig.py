132287a245b5256caee5631f8cf6cbe9c0bd36d7
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2015-01-25 13:37:45 +0100

    Daemonize

--- ifconfig.py
+++ ifconfig.py
@@ -1,9 +1,16 @@
+from daemon import DaemonContext
+from daemon.pidfile import TimeoutPIDLockFile
 from flask import Flask, request, Response
+from os.path import abspath, dirname, join
+
+pidfile_path = join(abspath(dirname(__file__)), 'ifconfig-py.pid')
 app = Flask(__name__)
+
 
 @app.route("/")
 def remote_addr():
     return Response(request.remote_addr, mimetype='text/plain; charset=utf-8')
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    with DaemonContext(pidfile=TimeoutPIDLockFile(pidfile_path)):
+        app.run()
