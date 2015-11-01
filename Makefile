ec09b67ca35005439036fd20f21195f898b95bcb
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2015-11-01 13:18:32 +0100

    Python smoketest for validating IPs
    Validating ipv4 AND ipv6 addresses would've been a pain in bash.

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ install-dependencies:
 	pip install -r requirements.txt
 
 deploy:
-	ssh ifconfig-py@oyvind.io 'cd ~/ifconfig.py && git pull && ~/env/bin/pip install -r requirements.txt && sudo systemctl reload-or-restart ifconfig-gunicorn.service && sudo systemctl status ifconfig-gunicorn.service && make smoketest'
+	ssh ifconfig-py@oyvind.io bash --login -c 'cd ~/ifconfig.py && git pull && make install-dependencies && sudo systemctl reload-or-restart ifconfig-gunicorn.service && sudo systemctl status ifconfig-gunicorn.service && make smoketest'
 
 smoketest:
-	[[ $$(curl -L http://oyvind.io/ip 2> /dev/null) =~ [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+ ]] && echo -e '\e[0;32mSmoketest OK\e[0m' || echo -e '\e[0;31mSmoketest FAILED\e[0m'
+	./smoketest.py
---
+++ smoketest.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import os
+import sys
+import requests
+import socket
+
+
+def ipaddr_response():
+    res = requests.get('http://oyvind.io/ip')
+    res.raise_for_status()
+    return res.content
+
+def is_valid_ipaddr(address_family, ipaddr):
+    try:
+        socket.inet_pton(address_family, ipaddr)
+        return True
+    except OSError:
+        return False
+
+if __name__ == '__main__':
+    if sys.stdout.isatty() and not os.getenv('TERM') == 'dumb':
+        def ok(): print('\033[92mSmoketest OK\033[0m')
+        def fail(): sys.exit('\033[91mSmoketest FAILED\033[0m')
+    else:
+        def ok(): print('Smoketest OK')
+        def fail(): sys.exit('Smoketest FAILED')
+
+    try:
+        ipaddr = ipaddr_response()
+    except requests.exceptions.HTTPError:
+        fail()
+
+    if is_valid_ipaddr(socket.AF_INET, ipaddr) or is_valid_ipaddr(socket.AF_INET6, ipaddr):
+        ok()
+    else:
+        fail()
